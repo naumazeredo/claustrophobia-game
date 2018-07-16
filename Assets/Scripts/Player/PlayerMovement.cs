9508768f39896226b418f1f447e9198a3552ecf4
Naum Azeredo <naumazeredo@gmail.com>
2018-07-16 06:16:34 -0300

    Add basic player movement and circle interaction

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+  public float speed = 150f;
+  public float circlePushForce = 10f;
+
+  Rigidbody2D rb;
+
+  GameObject circle;
+  CircleCollider2D circleCollider;
+  Rigidbody2D circleRigidbody;
+
+  float inputX;
+  float inputY;
+  float angle;
+
+  void Start () {
+    rb = GetComponent<Rigidbody2D>();
+
+    circle = GameObject.FindWithTag("Circle");
+    circleCollider = circle.GetComponent<CircleCollider2D>();
+    circleRigidbody = circle.GetComponent<Rigidbody2D>();
+  }
+
+  void Update () {
+    inputX = Input.GetAxis("Horizontal");
+    inputY = Input.GetAxis("Vertical");
+
+    // FIXME: Isso nao funciona se o objeto for ser empurrado por forcas externas...
+    Vector2 velocity = new Vector2(inputX, inputY) * speed;
+    rb.velocity = velocity;
+
+    // TODO: rotation speed?
+    if (Mathf.Max(Mathf.Abs(inputX), Mathf.Abs(inputY)) > 0.01f) {
+      angle = -Mathf.Atan2(inputX, inputY) * Mathf.Rad2Deg;
+      transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+  }
+
+  void FixedUpdate() {
+    Vector3 circlePlayerDist = transform.position - circle.transform.position;
+    if (circlePlayerDist.magnitude > circleCollider.radius) {
+      circlePlayerDist = circlePlayerDist.normalized * circleCollider.radius;
+      transform.position = circle.transform.position + circlePlayerDist;
+
+      circleRigidbody.AddForce(circlePushForce * circlePlayerDist.normalized);
+    }
+  }
+}
---
+++ ROADMAP.md
@@ -0,0 +1,44 @@
+# Roadmap
+
+- [ ] Player Movement
+  - [x] Basic (does not work with explosions...)
+  - [x] Space circle (using center of ship. Can be more sophisticated using the
+      colliders)
+  - [ ] React to explosions?
+- [ ] Attack
+  - [ ] Circle interaction
+  - [ ] Enemy interaction
+- [ ] Enemy
+  - [ ] Basic enemy movement
+  - [ ] Basic enemy attack
+  - [ ] First boss
+- [ ] Player health (circle shrink)
+  - [ ] Enemy attack interaction (invul time)
+- [ ] Level design
+  - [ ] Tutorial
+  - [ ] General level generation
+  - [ ] Level progression
+- [ ] Power ups
+- [ ] Graphics
+  - [ ] General graphics feel
+  - [ ] Player
+  - [ ] Circle
+  - [ ] Enemy
+    - [ ] Basic enemy
+    - [ ] Basic enemy attack
+  - [ ] Player attack
+- [ ] UI
+  - [ ] Basic UI
+- [ ] Extra
+  - [ ] More player attacks
+  - [ ] More enemies
+  - [ ] Boss low damage behaviour change
+  - [ ] Boss levels (more attacks, more movements)
+  - [ ] More power ups
+
+
+# BUGS
+
+- [ ] Unity
+  - [ ] "Screen position out of view frustum" -> Probably because of camera
+      size?
