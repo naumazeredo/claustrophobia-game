a82261d37068d2a159cf4d41af6cff4aee1d7e58
lucbarr <luc.s.barreira@gmail.com>
2018-07-17 19:56:23 -0300

    Add ZigZag enemy prefab and mover script

---
+++ Assets/Scripts/General/ZigZagMover.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class ZigZagMover : MonoBehaviour {
+  public float speedx = 2f;
+  public float speedy = 2f;
+  public float tilt = 45f;
+  public float tiltInterval = 1f;
+
+  Rigidbody2D rb;
+
+  void Start() {
+    rb = GetComponent<Rigidbody2D>();
+    rb.velocity = new Vector2(speedx*Mathf.Sin(Mathf.Deg2Rad*tilt), -speedy*Mathf.Cos(Mathf.Deg2Rad*tilt));
+    StartCoroutine(ZigZag());
+  }
+  IEnumerator ZigZag(){
+    while(true){
+      rb.velocity = new Vector2 (-rb.velocity.x, rb.velocity.y);
+      yield return new WaitForSeconds(tiltInterval);
+    }
+
+  }
+}
