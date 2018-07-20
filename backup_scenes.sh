a1f477bc8aeae73d4af12ef1ff9dadca109dea2c
Naum Azeredo <naumazeredo@gmail.com>
2018-07-20 03:57:19 -0300

    Fix QuadBoss behaviour

---
+++ backup_scenes.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf Temp/ScenesBackup
+mkdir -p Temp/ScenesBackup
+mv Assets/Scenes/* Temp/ScenesBackup/
---
+++ restore_scenes.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp Temp/ScenesBackup/* Assets/Scenes
