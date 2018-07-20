826bc79169b614a79e35ccd61607a8f3357724c4
Mateus de Castro <samuraiexx@gmail.com>
2018-07-20 03:22:05 -0300

    BOI shoot is now player's only option

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
   public float fireDelay = 0.2f;
-  public Shooting basicShooting;
 
-  public bool boiStyle;
-  public Shooting boiShooting;
+  public Shooting Shooting;
 
   GameController gameController;
 
@@ -22,24 +20,18 @@ public class PlayerAttack : MonoBehaviour {
 
   void Update () {
     if (!fired) {
-      if (!boiStyle) {
-        if (Input.GetButton("Fire")) {
-          Attack();
-        }
-      } else {
-        KeyMapper keyMapper = gameController.keyMapper;
-        inputX = Input.GetAxis(keyMapper.getHorizontalFire());
-        inputX = Mathf.Abs(inputX) < Mathf.Epsilon ? 0f : Mathf.Sign(inputX);
+      KeyMapper keyMapper = gameController.keyMapper;
+      inputX = Input.GetAxis(keyMapper.getHorizontalFire());
+      inputX = Mathf.Abs(inputX) < Mathf.Epsilon ? 0f : Mathf.Sign(inputX);
 
-        inputY = Input.GetAxis(keyMapper.getVerticalFire());
-        inputY = Mathf.Abs(inputY) < Mathf.Epsilon ? 0f : Mathf.Sign(inputY);
+      inputY = Input.GetAxis(keyMapper.getVerticalFire());
+      inputY = Mathf.Abs(inputY) < Mathf.Epsilon ? 0f : Mathf.Sign(inputY);
 
-        input = new Vector2(inputX, inputY);
-        input.Normalize();
+      input = new Vector2(inputX, inputY);
+      input.Normalize();
 
-        if (input != Vector2.zero) {
-          boiAttack(input);
-        }
+      if (input != Vector2.zero) {
+        Attack(input);
       }
     }
   }
@@ -51,13 +43,8 @@ public class PlayerAttack : MonoBehaviour {
     yield return null;
   }
 
-  void Attack() {
-    basicShooting.Fire();
-    StartCoroutine(Reload());
-  }
-
-  void boiAttack(Vector2 direction) {
-    boiShooting.Fire(direction);
+  void Attack(Vector2 direction) {
+    Shooting.Fire(direction);
     StartCoroutine(Reload());
   }
 }
